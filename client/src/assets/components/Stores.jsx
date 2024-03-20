e4e6bd18df73d8fa7fb3da94523110821c34a31c
saile <eliasiv.codes@gmail.com>
2024-03-19 21:10:25 -0700

    start stores page

--- client/src/assets/components/Stores.jsx
+++ client/src/assets/components/Stores.jsx
@@ -1,4 +1,16 @@
+import "./../scss/stores.scss";
+
 const Stores = () => {
-  return <div>Stores</div>;
+  return (
+    <div className="stores--container">
+      <div className="stores--item">
+        <header className="stores--name">Starbucks</header>
+        <div>
+          <p className="stores--saved-food">9 food</p>
+          <p className="stores--saved-drinks">13 drinks</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Stores;
